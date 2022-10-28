7972a2d2f9aa197b35ddf09fffbcfc185e98317a
Bohdan Pohorelov <pogorelov.play@gmail.com>
2022-10-29 00:05:06 +0300

    Create class Employee

---
+++ src/demo/Employee.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Employee {
+
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private int age;
+
+    public Employee(String name, String position, String email, String phone, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+    }
+}
