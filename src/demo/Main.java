e4a0727daf178e619fc6234e779ab33c4c3f12ae
Bohdan Pohorelov <pogorelov.play@gmail.com>
2022-11-01 19:06:15 +0200

    Add creating objects

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,10 +1,14 @@
 package demo;
 
+import sameName1.SameName;
+
 public class Main {
 
     public static void main(String[] args) {
         Car car = new Car();
         car.start();
+        SameName name = new SameName();
+        sameName2.SameName name2 = new sameName2.SameName();
 
         Employee employee = new Employee("Walter White", "teacher",
                 "met@hermano.com","050123456", 55);
